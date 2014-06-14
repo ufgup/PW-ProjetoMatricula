0410fb682218c7aa79d5743ae717e9a1bdb9acc4
Bruno Nogueira <ti.brunonogueira@gmail.com>
2014-06-13 21:30:27 -0300

    Inserindo pacote lab.utils

---
+++ src/main/java/lab/utils/JdbcUtils.java
@@ -0,0 +1,40 @@
+package lab.utils;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class JdbcUtils {
+	
+	public static Connection createConnection() {
+		Connection conn = null;
+		
+		try {
+			// URL de conexão JDBC com o banco de dados.
+			String url = "jdbc:derby://localhost/db;create=true";
+			// Obtém uma conexão com o banco de dados.
+			conn = DriverManager
+					.getConnection(url, "app", "123");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		return conn;
+	}
+	
+	
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			conn.close();
+			stmt.close();
+			rs.close();
+		} catch (Exception e) {
+			//Aqui já não dá para fazer nada
+		}
+	}
+	
+	public static void close(Connection conn, Statement stmt) {
+		close(conn, stmt, null);
+	}
+}
